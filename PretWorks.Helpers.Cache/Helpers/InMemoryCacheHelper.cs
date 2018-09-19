afd24692a131af0158ebce3fa85a0dc96a13e930
Rob van der Werf <rob@pretworks.com>
2018-09-19 09:16:15 +0200

    Inmemory cache helper

---
+++ PretWorks.Helpers.Cache/Helpers/InMemoryCacheHelper.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Threading.Tasks;
+using Microsoft.Extensions.Caching.Memory;
+using PretWorks.Helpers.Cache.Interfaces;
+
+namespace PretWorks.Helpers.Cache.Helpers
+{
+    public class InMemoryCacheHelper : ICachehelper
+    {
+        private readonly IMemoryCache _cache;
+
+        public InMemoryCacheHelper(IMemoryCache cache)
+        {
+            _cache = cache;
+        }
+
+        public async Task<T> GetOrAddAsync<T>(string key, Func<Task<T>> getter, int expiresInSeconds = 300)
+        {
+            if (_cache.TryGetValue(key, out T result))
+            {
+                return result;
+            }
+
+            result = await getter();
+
+            if (result == null)
+            {
+                return default;
+            }
+
+            _cache.Set(key, result, new TimeSpan(0, 0, expiresInSeconds));
+
+            return result;
+        }
+
+        public void Remove(string key)
+        {
+            _cache.Remove(key);
+        }
+    }
+}
---
+++ PretWorks.Helpers.Cache/Interfaces/ICacheHelper.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Threading.Tasks;
+
+namespace PretWorks.Helpers.Cache.Interfaces
+{
+    public interface ICachehelper
+    {
+        /// <summary>
+        /// Get or add T from the cache
+        /// </summary>
+        /// <typeparam name="T"></typeparam>
+        /// <param name="key">Key to store the item</param>
+        /// <param name="getter">Func to get the item if not in cache</param>
+        /// <param name="expiresInSeconds">Lifetime of the new item in cache</param>
+        /// <returns></returns>
+        Task<T> GetOrAddAsync<T>(string key, Func<Task<T>> getter, int expiresInSeconds = 300);
+
+        /// <summary>
+        /// Remove item from cache
+        /// </summary>
+        /// <param name="key"></param>
+        void Remove(string key);
+    }
+}
