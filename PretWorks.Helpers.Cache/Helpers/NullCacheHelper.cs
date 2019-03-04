94d44767fb3b8aef1cd8896c938b10f271cc9a49
Rob van der Werf <rob@pretworks.com>
2019-03-04 09:02:29 +0100

    NullCacheHelper for unittesting

---
+++ PretWorks.Helpers.Cache/Helpers/NullCacheHelper.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Threading.Tasks;
+using PretWorks.Helpers.Cache.Interfaces;
+
+namespace PretWorks.Helpers.Cache.Helpers
+{
+    public class NullCacheHelper : ICachehelper
+    {
+        public Task<T> GetOrAddAsync<T>(string key, Func<Task<T>> getter, int expiresInSeconds = 300)
+        {
+            return getter();
+        }
+
+        public void Remove(string key)
+        {
+            
+        }
+    }
+}
